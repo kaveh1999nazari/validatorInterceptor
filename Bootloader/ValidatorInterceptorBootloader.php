c349a5d8b5cc73747b3fed70b32f51f4760e04b8
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-04 10:54:45 +0330

    first

--- Bootloader/ValidatorInterceptorBootloader.php
+++ Bootloader/ValidatorInterceptorBootloader.php
@@ -3,14 +3,14 @@
 namespace App\src\Bootloader;
 
 use Spiral\Boot\Bootloader\Bootloader;
-use Spiral\Core\Container\SingletonInterface;
 use Spiral\Config\ConfiguratorInterface;
 use Spiral\Config\Patch\Append;
 
-class ValidatorInterceptorBootloader extends Bootloader implements SingletonInterface
+class ValidatorInterceptorBootloader extends Bootloader
 {
     public function __construct(ConfiguratorInterface $config)
     {
-        $config->modify('grpc', new Append('interceptor', \App\Endpoint\Interceptor\ValidatorInterceptor::class));
+        $config->modify('grpc', new Append('interceptor', null, \App\Endpoint\Interceptor\ValidatorInterceptor::class));
     }
 }
+

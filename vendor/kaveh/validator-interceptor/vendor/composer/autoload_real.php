f502c5c96ca3ed7a6032aae676cbbd8c19e26fe5
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-04 10:44:41 +0330

    first

--- vendor/kaveh/validator-interceptor/vendor/composer/autoload_real.php
+++ vendor/kaveh/validator-interceptor/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit04ee5e35bddbd768479a8a7538e1c35e
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit04ee5e35bddbd768479a8a7538e1c35e::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }

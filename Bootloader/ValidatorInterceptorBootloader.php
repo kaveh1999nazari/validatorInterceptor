48df8a9b03b05534f336b181468c379a66b56a31
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-11-04 11:40:44 +0330

    first

--- Bootloader/ValidatorInterceptorBootloader.php
+++ Bootloader/ValidatorInterceptorBootloader.php
@@ -1,16 +1,51 @@
 <?php
 
-namespace App\src\Bootloader;
+namespace Vendor\Package\Bootloader;
 
 use Spiral\Boot\Bootloader\Bootloader;
 use Spiral\Config\ConfiguratorInterface;
 use Spiral\Config\Patch\Append;
+use Psr\Container\ContainerInterface;
 
-class ValidatorInterceptorBootloader extends Bootloader
+class PackageBootloader extends Bootloader
 {
-    public function __construct(ConfiguratorInterface $config)
+    /**
+     * @var ConfiguratorInterface
+     */
+    private ConfiguratorInterface $config;
+
+    /**
+     * @var ContainerInterface
+     */
+    private ContainerInterface $container;
+
+    /**
+     * Constructor for PackageBootloader.
+     *
+     * @param ConfiguratorInterface $config
+     * @param ContainerInterface $container
+     */
+    public function __construct(ConfiguratorInterface $config, ContainerInterface $container)
     {
-        $config->modify('grpc', new Append('interceptor', null, \App\Endpoint\Interceptor\ValidatorInterceptor::class));
+        $this->config = $config;
+        $this->container = $container;
+    }
+
+    /**
+     * The boot method is executed when the package is booted.
+     */
+    public function boot(): void
+    {
+    }
+
+    /**
+     * Initialize package configurations or other initial setup.
+     */
+    public function init(): void
+    {
+        $this->config->modify('grpc',
+        new Append('interceptor', \App\Endpoint\Interceptor\ValidatorInterceptor::class)
+        );
     }
 }
 
